2864bb1db98d823e91f2d81edf0e89eefc373b43
busrademirbasozkan <busrademirbasozkan@gmail.com>
2022-11-08 15:52:04 +0300

    UITableView'in doğru çalışması için gerekli protokol ve fonksiyonlar eklendi.

--- LandmarkBook/Controller/ViewController.swift
+++ LandmarkBook/Controller/ViewController.swift
@@ -7,11 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Bu şekilde yazarak delegate ve datasource yetkisinide viewcontrollera vermiş oluyoruz.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    // UITableView kullanmak istiyorsak bu iki fonksiyonu çalıştırmamız zorunlu. Ancak UITableViewDelegate ve UITableViewDataSource protokollerini eklemezsek bu fonksiyonlar çalışmaz.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "test"
+        return cell
     }
 
 

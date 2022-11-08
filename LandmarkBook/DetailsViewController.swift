8ff103260b556e697666c98331c285640e8bed98
busrademirbasozkan <busrademirbasozkan@gmail.com>
2022-11-08 18:02:00 +0300

    DetailsViewController eklendi.

---
+++ LandmarkBook/DetailsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DetailsViewController.swift
+//  LandmarkBook
+//
+//  Created by Büşra Özkan on 8.11.2022.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet weak var landmarkName: UILabel!
+    @IBOutlet weak var landmarkImage: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
